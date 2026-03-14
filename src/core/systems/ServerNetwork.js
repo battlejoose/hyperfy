fbe487b1f89e6108baa1bf7246f3923ffbbbeded
CrowBrow <battlejoose@gmail.com>
2026-03-13 20:42:50 -0600

    all lobsters

--- src/core/systems/ServerNetwork.js
+++ src/core/systems/ServerNetwork.js
@@ -261,7 +261,7 @@ export class ServerNetwork extends System {
       const isAgent = !!params.agentKey
 
       // spawn player
-      const defaultAvatar = isAgent ? 'asset://lobster.vrm' : (this.world.settings.avatar?.url || 'asset://avatar.vrm')
+      const defaultAvatar = 'asset://lobster.vrm'
       socket.player = this.world.entities.add(
         {
           id: user.id,
