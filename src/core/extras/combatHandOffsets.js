1a9d42df0ac04a3596e95516fd2ab8334fe76450
battlejoose <battlejoose@gmail.com>
2026-06-07 10:28:27 -0600

    Update combatHandOffsets.js

--- src/core/extras/combatHandOffsets.js
+++ src/core/extras/combatHandOffsets.js
@@ -14,7 +14,7 @@ export const CombatHandOffsets = {
   attackLeft: null,
   attackRight: null,
   attackHigh: null,
-  attackLow: { x: 20, y: 0, z: -45 },
+  attackLow: { x: 30, y: 0, z: -45 },
   block: null,
   blockLeft: null,
   blockRight: null,
